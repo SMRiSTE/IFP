d031c6ce751ba50493d813153c69229e0515bfd1
SMRist <s.m.r.00800ist@gmail.com>
2024-07-11 01:41:47 +0300

    Initial commit

--- IFP/IFP/IFP.cpp
+++ IFP/IFP/IFP.cpp
@@ -6,13 +6,8 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     
-    try {
-        parser parser("ini.txt");
-        parser.get_vec();
-        auto value = parser.get_value<int>("Section1");
-        std::cout << "Value: " << value << std::endl;
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    parser parser("ini.txt");
+    parser.get_vec();
+    auto value = parser.get_value<int>("Section1");
+    std::cout << "Value: " << value << std::endl;
 }
